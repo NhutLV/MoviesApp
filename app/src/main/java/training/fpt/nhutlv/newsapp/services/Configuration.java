746d6dc0f674be1a9f94eb783e428750c5053821
NhutLV <nhut.lv.it@gmail.com>
2016-12-19 01:04:43 +0700

    add configuration

---
+++ app/src/main/java/training/fpt/nhutlv/newsapp/services/Configuration.java
@@ -0,0 +1,24 @@
+package training.fpt.nhutlv.newsapp.services;
+
+import java.net.URL;
+
+/**
+ * Created by NhutDu on 18/12/2016.
+ */
+
+public class Configuration {
+
+    private static final String URL_BASE_IMAGE ="https://api.themoviedb.org/3/configuration?api_key=6cb1ab70342ce4b11dc26b59fdf7e442";
+    private static final String URL_MOVIE_POPULAR ="https://api.themoviedb.org/3/movie/popular?api_key=6cb1ab70342ce4b11dc26b59fdf7e442&language=en";
+    private static final String URL_MOVIE_TOP_RATED ="https://api.themoviedb.org/3/movie/popular?api_key=6cb1ab70342ce4b11dc26b59fdf7e442&language=en";
+    private static final String URL_MOVIE_UPCOMING ="https://api.themoviedb.org/3/movie/popular?api_key=6cb1ab70342ce4b11dc26b59fdf7e442&language=en";
+    private static final String URL_MOVIE_NOW_PLAYING ="https://api.themoviedb.org/3/movie/popular?api_key=6cb1ab70342ce4b11dc26b59fdf7e442&language=en";
+
+
+    private static final String URL_TV_POPULAR ="https://api.themoviedb.org/3/tv/popular?api_key=6cb1ab70342ce4b11dc26b59fdf7e442&language=en";
+    private static final String URL_TV_TOP_RATED ="https://api.themoviedb.org/3/tv/popular?api_key=6cb1ab70342ce4b11dc26b59fdf7e442&language=en";
+    private static final String URL_TV_UPCOMING ="https://api.themoviedb.org/3/tv/popular?api_key=6cb1ab70342ce4b11dc26b59fdf7e442&language=en";
+    private static final String URL_TV_NOW_PLAYING ="https://api.themoviedb.org/3/tv/popular?api_key=6cb1ab70342ce4b11dc26b59fdf7e442&language=en";
+
+
+}
